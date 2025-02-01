d596c545c60062082211408345609bd800dbf799
Leonardo Rudolf Manangka <leomanangka@outlook.com>
2025-02-01 18:29:48 +0700

    add implementation for wifi sta mode function

--- src/main.c
+++ src/main.c
@@ -1,1 +1,12 @@
-void app_main() {}
+#include "esp_log.h"
+#include "wifi_sta.h"
+
+void app_main() {
+  static const char *MAIN_TAG = "main_tag";
+  wifi_init_sta();
+  if (isConnected()) {
+    ESP_LOGI(MAIN_TAG, "WiFi Is Connected!!");
+  } else {
+    ESP_LOGI(MAIN_TAG, "WiFi Is Not Connected!!");
+  }
+}
